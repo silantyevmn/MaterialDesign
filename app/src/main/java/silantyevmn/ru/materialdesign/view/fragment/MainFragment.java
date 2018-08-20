b6cb8e078feafc775778b74135724df6b0393a1d
silantyevmn <5992033@mail.ru>
2018-08-20 18:16:20 +0300

    delete TextView add Recycler

--- app/src/main/java/silantyevmn/ru/materialdesign/view/fragment/MainFragment.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/fragment/MainFragment.java
@@ -1,25 +1,35 @@
 package silantyevmn.ru.materialdesign.view.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.List;
 
 import silantyevmn.ru.materialdesign.R;
+import silantyevmn.ru.materialdesign.model.MainRecyclerAdapter;
 import silantyevmn.ru.materialdesign.model.entity.Enity;
 import silantyevmn.ru.materialdesign.model.entity.Photo;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements MainRecyclerAdapter.onClick{
     private static String ARG_PHOTOS = "photos";
+    private final int COUNT_SPAN=3; //количество фото в строке
     private List<Photo> photos;
-    private TextView container;
+    private RecyclerView recyclerView;
+    private MainRecyclerAdapter adapter;
 
     public static MainFragment newInstance(List<Photo> photos) {
         MainFragment fragment = new MainFragment();
@@ -42,19 +52,31 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        this.container = rootView.findViewById(R.id.container);
+        recyclerView = rootView.findViewById(R.id.recycler);
         return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        showView();
+        init();
+        initAdapter(photos);
     }
 
-    private void showView() {
-        for (Enity photo : photos) {
-            container.append(photo.getName() + "\n");
-        }
+    private void init() {
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(getContext(),COUNT_SPAN);
+        recyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    private void initAdapter(List<Photo> photos) {
+        adapter = new MainRecyclerAdapter(photos,this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClickRecyclerAdapter(int position) {
+        //todo заглушка
+        Snackbar mSnackbar = Snackbar.make(getView(), "position-"+position, Snackbar.LENGTH_LONG);
+        mSnackbar.show();
     }
 }
