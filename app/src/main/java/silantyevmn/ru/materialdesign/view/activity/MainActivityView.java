9c453dfbf183ababf81cb025d0ce96b8c08b7b29
silantyevmn <5992033@mail.ru>
2018-08-25 23:12:39 +0300

    new interface

---
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/MainActivityView.java
@@ -0,0 +1,15 @@
+package silantyevmn.ru.materialdesign.view.activity;
+
+import java.util.List;
+
+import silantyevmn.ru.materialdesign.model.photo.Photo;
+import silantyevmn.ru.materialdesign.model.theme.Theme;
+
+/**
+ * Created by silan on 24.08.2018.
+ */
+
+public interface MainActivityView {
+    void showFragment(List<Photo> photos);
+    void showSetting(List<Theme> themes);
+}
