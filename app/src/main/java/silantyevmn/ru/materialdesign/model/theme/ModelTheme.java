ed6b43296b28c946b8c42d858d3cab843ed3073a
silantyevmn <5992033@mail.ru>
2018-08-25 23:15:53 +0300

    new class ModelTheme

---
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/theme/ModelTheme.java
@@ -0,0 +1,31 @@
+package silantyevmn.ru.materialdesign.model.theme;
+
+import java.util.List;
+
+/**
+ * Created by silan on 18.08.2018.
+ */
+
+public class ModelTheme {
+    private static ModelTheme modelTheme;
+    private IModelTheme iModel;
+
+    public ModelTheme(){
+        iModel=new ThemeEmmiter();
+    }
+
+    public List<Theme> getList() {
+        return iModel.getList();
+    }
+
+    public static ModelTheme getInstance(){
+        return modelTheme;
+    }
+
+    public static void init() {
+        if(modelTheme==null){
+            modelTheme=new ModelTheme();
+        }
+    }
+
+}
