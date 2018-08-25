d5b4e3861f0e97ee62c4e4faeabe9175294569b3
silantyevmn <5992033@mail.ru>
2018-08-25 23:16:52 +0300

    rename package rename field

--- app/src/main/java/silantyevmn/ru/materialdesign/model/photo/ModelPhoto.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/photo/ModelPhoto.java
@@ -1,41 +1,37 @@
-package silantyevmn.ru.materialdesign.model;
+package silantyevmn.ru.materialdesign.model.photo;
 
 import java.util.List;
-
-import silantyevmn.ru.materialdesign.model.entity.IPhotoEmmiter;
-import silantyevmn.ru.materialdesign.model.entity.Photo;
-import silantyevmn.ru.materialdesign.model.entity.PhotoEmmiter;
 
 /**
  * Created by silan on 18.08.2018.
  */
 
 public class ModelPhoto implements IModelPhoto<Photo> {
-    private IPhotoEmmiter iModel;
+    private IPhotoEmmiter iPhotoEmmiter;
     private static ModelPhoto modelPhoto;
 
     private ModelPhoto(){
-        iModel=new PhotoEmmiter();
+        iPhotoEmmiter =new PhotoEmmiter();
     }
 
     @Override
     public List<Photo> getList() {
-        return iModel.getList();
+        return iPhotoEmmiter.getList();
     }
 
     @Override
     public void insert(Photo photo) {
-        iModel.insert(photo);
+        iPhotoEmmiter.insert(photo);
     }
 
     @Override
     public void delete(Photo photo) {
-        iModel.delete(photo);
+        iPhotoEmmiter.delete(photo);
     }
 
     @Override
     public void favorites(Photo photo) {
-        iModel.favorites(photo);
+        iPhotoEmmiter.favorites(photo);
     }
 
     public static ModelPhoto getInstance(){
