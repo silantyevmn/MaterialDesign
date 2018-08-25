54df4b019ccb7d5dac0fbd2cbd0438721dccb4d2
silantyevmn <5992033@mail.ru>
2018-08-25 23:13:50 +0300

    add presenter MVP

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/MainActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/MainActivity.java
@@ -2,7 +2,6 @@ package silantyevmn.ru.materialdesign.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,67 +10,26 @@ import java.io.Serializable;
 import java.util.List;
 
 import silantyevmn.ru.materialdesign.R;
-import silantyevmn.ru.materialdesign.controler.MainController;
 import silantyevmn.ru.materialdesign.model.DataSharedPreference;
-import silantyevmn.ru.materialdesign.model.entity.Photo;
-import silantyevmn.ru.materialdesign.model.entity.Theme;
-import silantyevmn.ru.materialdesign.view.fragment.MainFragment;
+import silantyevmn.ru.materialdesign.model.photo.Photo;
+import silantyevmn.ru.materialdesign.model.theme.Theme;
+import silantyevmn.ru.materialdesign.presenter.MainPresenter;
+import silantyevmn.ru.materialdesign.view.fragment.PhotoFragment;
 
-public class MainActivity extends AppCompatActivity implements MainController.SetViewActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityView{
+    private final MainPresenter presenter;
+    private final int SETTING_REQUEST = 2;
 
-    private MainController controller;
-    private final Handler handler = new Handler();
+    public MainActivity(){
+        presenter=new MainPresenter(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(DataSharedPreference.getInstance().getCurrentTheme());
         setContentView(R.layout.activity_main);
-        init();
-    }
-
-    private void init() {
-        this.controller = new MainController(this);
-    }
-
-    @Override
-    public void onShowFragmentPhotos(final List<Photo> photos) {
-        //adapter.setData(photos);
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                createFragment(photos);
-            }
-        });
-
-    }
-
-    public void onShowActivitySetting(final List<Theme> themes) {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                intent.putExtra(SettingsActivity.LIST_THEMES, (Serializable) themes);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            recreate();
-        }
-    }
-
-    private void createFragment(List<Photo> photos) {
-        MainFragment mainFragment = MainFragment.newInstance(photos);
-        //запускаем транзакцию и добавляем фрагмент
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_details, mainFragment)
-                .commit();
+        presenter.onCreate();
     }
 
     @Override
@@ -82,22 +40,28 @@ public class MainActivity extends AppCompatActivity implements MainController.Se
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // получим идентификатор выбранного пункта меню
-        int id = item.getItemId();
-        // Операции для выбранного пункта меню
-        switch (id) {
-            case R.id.action_settings: {
-                controller.settingsSelected();
-                return true;
-            }
-            default:
-                return false;
-        }
+        return presenter.onClickMenuItem(item);
     }
 
     @Override
-    protected void onDestroy() {
-        controller.setViewActivity(null);
-        super.onDestroy();
+    public void showFragment(List<Photo> photos) {
+        PhotoFragment mainFragment = new PhotoFragment();
+        //запускаем транзакцию и добавляем фрагмент
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_details, mainFragment)
+                .commit();
+    }
+
+    @Override
+    public void showSetting(List<Theme> themes) {
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        intent.putExtra(SettingsActivity.LIST_THEMES, (Serializable) themes);
+        startActivityForResult(intent,SETTING_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        recreate();
     }
 }
