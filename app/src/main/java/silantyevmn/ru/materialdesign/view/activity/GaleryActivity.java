fd2360c7d2a1d32ff42254f615030a841c62c3c9
silantyevmn <5992033@mail.ru>
2018-08-26 19:37:56 +0300

    add NavigationDrawer

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
@@ -1,67 +1,101 @@
 package silantyevmn.ru.materialdesign.view.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.Serializable;
-import java.util.List;
-
-import silantyevmn.ru.materialdesign.R;
-import silantyevmn.ru.materialdesign.model.DataSharedPreference;
-import silantyevmn.ru.materialdesign.model.photo.Photo;
-import silantyevmn.ru.materialdesign.model.theme.Theme;
-import silantyevmn.ru.materialdesign.presenter.GaleryPresenter;
-import silantyevmn.ru.materialdesign.view.fragment.PhotoFragment;
-
-public class GaleryActivity extends AppCompatActivity implements IGaleryView {
-    private final GaleryPresenter presenter;
-    private final int SETTING_REQUEST = 2;
-
-    public GaleryActivity(){
-        presenter=new GaleryPresenter(this);
-    }
+public class Main2Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(DataSharedPreference.getInstance().getCurrentTheme());
-        setContentView(R.layout.activity_main);
-        presenter.onCreate();
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return presenter.onClickMenuItem(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void showFragment(List<Photo> photos) {
-        PhotoFragment mainFragment = new PhotoFragment();
-        //запускаем транзакцию и добавляем фрагмент
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_details, mainFragment)
-                .commit();
-    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-    @Override
-    public void showSetting(List<Theme> themes) {
-        Intent intent = new Intent(GaleryActivity.this, SettingsActivity.class);
-        intent.putExtra(SettingsActivity.LIST_THEMES, (Serializable) themes);
-        startActivityForResult(intent,SETTING_REQUEST);
-    }
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        recreate();
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
