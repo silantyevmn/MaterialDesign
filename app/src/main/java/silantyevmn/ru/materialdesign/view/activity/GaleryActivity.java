0a268b523a5385167ec4cc0554201a20bf7c0a5e
silantyevmn <5992033@mail.ru>
2018-08-27 16:09:59 +0300

    delete activity FloatingActionButton

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
@@ -118,9 +118,6 @@ public class GaleryActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> presenter.onClickFab());
-
         drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
