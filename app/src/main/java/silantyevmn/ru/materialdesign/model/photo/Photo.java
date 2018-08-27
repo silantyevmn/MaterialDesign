59c379218ffca0fd17d09ffd0fe669782048c6d9
silantyevmn <5992033@mail.ru>
2018-08-27 16:13:05 +0300

    delete Serialazeble

--- app/src/main/java/silantyevmn/ru/materialdesign/model/photo/Photo.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/photo/Photo.java
@@ -2,39 +2,33 @@ package silantyevmn.ru.materialdesign.model.photo;
 
 import android.net.Uri;
 
-import java.io.Serializable;
-
 /**
  * Created by silan on 18.08.2018.
  */
 
-public class Photo implements Serializable {
+public class Photo {
     private String name;
-    private boolean isFavorites;
-    private Uri localUri=null;
+    private boolean isFavorite;
+    private Uri uri=null;
 
     public Photo(String name) {
-        this.name=name;
+        this.name = name;
     }
 
-    public Photo(Uri uri){
-        this.localUri=uri;
+    public boolean isFavorite() {
+        return isFavorite;
     }
 
-    public boolean isFavorites() {
-        return isFavorites;
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
     }
 
-    public void setFavorites(boolean favorites) {
-        isFavorites = favorites;
+    public Uri getUri() {
+        return uri;
     }
 
-    public Uri getLocalUri() {
-        return localUri;
-    }
-
-    public void setLocalUri(Uri localUri) {
-        this.localUri = localUri;
+    public void setUri(Uri uri) {
+        this.uri = uri;
     }
 
     public String getName() {
