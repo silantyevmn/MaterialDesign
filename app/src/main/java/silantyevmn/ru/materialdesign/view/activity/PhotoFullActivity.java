da7dafac65694ffea0fdc82230fbfeaed1f79306
silantyevmn <5992033@mail.ru>
2018-08-30 14:07:54 +0300

    новое активити для отображение фотографии

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/PhotoFullActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/PhotoFullActivity.java
@@ -1,13 +1,28 @@
 package silantyevmn.ru.materialdesign.view.activity;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import silantyevmn.ru.materialdesign.R;
+import silantyevmn.ru.materialdesign.model.DataSharedPreference;
 
 public class PhotoFullActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(DataSharedPreference.getInstance().getCurrentTheme());
         setContentView(R.layout.activity_photo_full);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //добавляем в АппБар кнопку назад
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ImageView imageView=findViewById(R.id.image_full);
+        Uri uri=Uri.parse((getIntent().getStringExtra("image_full")));
+        imageView.setImageURI(uri);
     }
 }
