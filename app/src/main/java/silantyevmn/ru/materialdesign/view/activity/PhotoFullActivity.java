cdd400c81f405ada26b8701633e1aa8c12446085
silantyevmn <5992033@mail.ru>
2018-08-30 14:07:42 +0300

    новое активити для отображение фотографии

---
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/PhotoFullActivity.java
@@ -0,0 +1,13 @@
+package silantyevmn.ru.materialdesign.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoFullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_full);
+    }
+}
