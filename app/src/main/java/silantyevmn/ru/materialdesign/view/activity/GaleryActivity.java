04ec8dc9d315fe8794716c98b1a2460a6b28f87c
silantyevmn <5992033@mail.ru>
2018-08-30 14:06:45 +0300

    удалил import android.support.design.widget.FloatingActionButton;

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
@@ -2,7 +2,6 @@ package silantyevmn.ru.materialdesign.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
