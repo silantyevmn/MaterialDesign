cdc753a8eab2590c6f21ea14c232ff342dee1c8d
silantyevmn <5992033@mail.ru>
2018-08-30 14:05:10 +0300

    внес загрузку картинок из хранилища

--- app/src/main/java/silantyevmn/ru/materialdesign/model/photo/PhotoEmmiter.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/photo/PhotoEmmiter.java
@@ -1,17 +1,21 @@
 package silantyevmn.ru.materialdesign.model.photo;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import silantyevmn.ru.materialdesign.model.DataSharedPreference;
 
 /**
  * Created by silan on 19.08.2018.
  */
 
 public class PhotoEmmiter implements IPhotoEmmiter {
-    private String[] photoName = new String[]{"фото1", "фото2", "фото3", "фото4", "фото5", "фото6", "фото7"};
     private List<Photo> arrays;
+    private FileOperation fileOperation;
 
     public PhotoEmmiter(){
+        fileOperation=FileOperation.getInstance();
         init();
     }
 
@@ -21,15 +25,24 @@ public class PhotoEmmiter implements IPhotoEmmiter {
     }
 
     public void init() {
+        //content://com.example.android.provider/my_images/JPEG_20180829_143946_1411028100.jpg
         arrays = new ArrayList<>();
-        for (int i = 0; i < photoName.length; i++) {
-            arrays.add(new Photo(photoName[i]));
+        for (File file : fileOperation.getStorageDir().listFiles()) {
+            Photo photo=new Photo(file.getName(),fileOperation.getUri(file).toString());
+            boolean favorite=DataSharedPreference.getInstance().getFavorite(photo.getName());
+            if(favorite){
+                photo.setFavorite(true);
+            } else
+                photo.setFavorite(false);
+            arrays.add(photo);
         }
     }
 
     @Override
     public void delete(Photo photo) {
         arrays.remove(photo);
+        fileOperation.deleteFile(photo);
+        DataSharedPreference.getInstance().deleteFavorite(photo.getName());
     }
 
     @Override
@@ -37,12 +50,14 @@ public class PhotoEmmiter implements IPhotoEmmiter {
         int position=arrays.indexOf(photo);
         photo.setFavorite(!photo.isFavorite());
         arrays.set(position,photo);
+        DataSharedPreference.getInstance().setFavorite(photo.getName(),photo.isFavorite());
     }
 
 
     @Override
     public void insert(Photo photo) {
         arrays.add(photo);
+        fileOperation.insert(photo);
     }
 
 
