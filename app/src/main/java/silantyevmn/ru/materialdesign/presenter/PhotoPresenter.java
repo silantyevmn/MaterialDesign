bf7f051731fa558364eeb9860602f0b4d4052971
silantyevmn <5992033@mail.ru>
2018-09-05 01:11:02 +0300

    refactoring code added check for deleting temporary file from camera

--- app/src/main/java/silantyevmn/ru/materialdesign/presenter/PhotoPresenter.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/presenter/PhotoPresenter.java
@@ -1,17 +1,13 @@
 package silantyevmn.ru.materialdesign.presenter;
 
-import android.content.Intent;
 import android.net.Uri;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import silantyevmn.ru.materialdesign.R;
 import silantyevmn.ru.materialdesign.model.DataSharedPreference;
-import silantyevmn.ru.materialdesign.model.photo.FileOperation;
 import silantyevmn.ru.materialdesign.model.photo.IModelPhoto;
-import silantyevmn.ru.materialdesign.model.photo.ModelPhoto;
+import silantyevmn.ru.materialdesign.model.photo.PhotoModel;
 import silantyevmn.ru.materialdesign.model.photo.Photo;
 import silantyevmn.ru.materialdesign.view.DialogView;
 import silantyevmn.ru.materialdesign.view.fragment.IPhotoFragment;
@@ -27,11 +23,11 @@ public class PhotoPresenter {
 
     public PhotoPresenter(PhotoFragment photoFragment) {
         this.view = photoFragment;
-        model = ModelPhoto.getInstance();
+        model = PhotoModel.getInstance();
     }
     //создание View
-    public void onViewCreated() {
-        view.init(getPhotos());
+    public void onViewCreated(int span) {
+        view.init(getPhotos(),span);
     }
 
     private List<Photo> getPhotos() {
@@ -42,11 +38,13 @@ public class PhotoPresenter {
         view.setAdapter(getPhotos());
     }
 
-    public void addPhoto() {
-        view.showGalery();
-    }
-
     public void delete(int position) {
+        if(position==-1){
+            Uri uri= Uri.parse(DataSharedPreference.getInstance().getUriCamera());
+            Photo photo = new Photo(uri.getLastPathSegment(),uri.toString());
+            model.delete(photo);
+            return;
+        }
         new DialogView(view.getActivity(), view.getActivity().getString(R.string.dialog_title_delete), () -> {
             model.delete(getPhotos().get(position));
             updateAdapter();
@@ -79,11 +77,10 @@ public class PhotoPresenter {
         view.showFullPhoto(getPhotos().get(adapterPosition));
     }
 
-    public void insertGalery(String s) {
-        //todo нужно сохранить в директорию, а как вытащить имя файла пока не знаю...
-        Photo photo=new Photo(Uri.parse(s).getLastPathSegment(),s);
+    public void insertGalery(String uriString) {
+        Photo photo=new Photo(Uri.parse(uriString).getLastPathSegment(),uriString);
         model.insert(photo);
-        view.showLog("insertCamera", String.valueOf(getPhotos().size()));
+        view.showLog("insertGalery", String.valueOf(getPhotos().size()));
         updateAdapter();
     }
 }
