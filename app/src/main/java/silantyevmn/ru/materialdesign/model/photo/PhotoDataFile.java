84173f2e045d705332bbb85397ddbdbca35b0d05
silantyevmn <5992033@mail.ru>
2018-09-05 01:12:48 +0300

    rename class FileOperation -> PhotoDataFile

--- app/src/main/java/silantyevmn/ru/materialdesign/model/photo/PhotoDataFile.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/photo/PhotoDataFile.java
@@ -3,32 +3,32 @@ package silantyevmn.ru.materialdesign.model.photo;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
+import silantyevmn.ru.materialdesign.R;
 
 /**
  * Created by silan on 29.08.2018.
  */
 
-public class FileOperation {
-    private static FileOperation fileOperation;
+public class PhotoDataFile implements IPhotoEmmiter {
+    private static PhotoDataFile fileOperation;
     private final File storageDIR;
-    private final String path="content://com.example.android.provider/my_images/";
+    //private final String path = "content://com.example.android.provider/my_images/";
     private File photoFile;
 
-    public File getStorageDir() {
-        return storageDIR;
+    private PhotoDataFile(Context context) {
+        this.storageDIR = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
     }
 
-    public String getPath() {
-        return path;
-    }
-
-    private FileOperation(Context context) {
-        this.storageDIR = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);;
+    public Uri getUriToFileProvider(Context context,File file){
+        return FileProvider.getUriForFile(context, context.getString(R.string.file_provider),photoFile);
     }
 
     public File createImageFile() throws IOException {
@@ -42,36 +42,46 @@ public class FileOperation {
         return photoFile;
     }
 
-    public static FileOperation getInstance(){
-        return fileOperation;
-    }
-
-    public static void init(Context context){
-        if(fileOperation==null){
-            fileOperation=new FileOperation(context);
-        }
-    }
-
     public Uri getUri(File photoFile) {
-        return Uri.parse(path+photoFile.getName());
+        //return Uri.parse(path + photoFile.getName());
+        return Uri.fromFile(photoFile);
     }
 
-    public void deleteFile(Photo photo) {
+    @Override
+    public List<Photo> getList() {
+        return null;
+    }
+
+    @Override
+    public void insert(Photo photo) {
+    }
+
+    @Override
+    public void delete(Photo photo) {
         for (File file : storageDIR.listFiles()) {
-            if(file.getName().equals(photo.getName())){
+            if (file.getName().equals(photo.getName())) {
                 file.delete();
                 return;
             }
         }
     }
 
-    public boolean deleteNewFile() {
-        return photoFile.delete();
+    @Override
+    public void favorites(Photo photo) {
+
     }
 
-    public void insert(Photo photo) {
-        File newFile= new File(photo.getName());
+    public static PhotoDataFile getInstance() {
+        return fileOperation;
+    }
 
+    public static void init(Context context) {
+        if (fileOperation == null) {
+            fileOperation = new PhotoDataFile(context);
+        }
+    }
 
+    public File getStorageDir() {
+        return storageDIR;
     }
 }
