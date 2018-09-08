2c086191b07da595dd24fc0cf23f9a6baa77cc2e
silantyevmn <5992033@mail.ru>
2018-09-09 00:17:37 +0300

    новые интерфейсы

---
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/photo/IPhotoAdapter.java
@@ -0,0 +1,11 @@
+package silantyevmn.ru.materialdesign.model.photo;
+
+import java.util.List;
+
+/**
+ * Created by silan on 08.09.2018.
+ */
+
+public interface IPhotoAdapter {
+    void setAdapter(List<Photo> photos);
+}
