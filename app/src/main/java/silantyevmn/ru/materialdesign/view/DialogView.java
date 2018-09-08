a1b4c22922eb6abfc210db1fa03e3e123de0e4f9
silantyevmn <5992033@mail.ru>
2018-09-09 00:26:23 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/materialdesign/view/DialogView.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/DialogView.java
@@ -1,7 +1,6 @@
 package silantyevmn.ru.materialdesign.view;
 
 import android.app.Activity;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
 import silantyevmn.ru.materialdesign.R;
