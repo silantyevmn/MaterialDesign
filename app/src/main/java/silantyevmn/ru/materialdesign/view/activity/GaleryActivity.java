9956dda3dfddf137f246b05f3aae19783f2629dc
silantyevmn <5992033@mail.ru>
2018-09-11 14:26:12 +0300

    добавил метод showFullPhoto, фабрику фрагментов и viewPager

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/GaleryActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,12 +22,11 @@ import java.util.List;
 
 import silantyevmn.ru.materialdesign.R;
 import silantyevmn.ru.materialdesign.model.DataSharedPreference;
-import silantyevmn.ru.materialdesign.model.photo.IPhotoAdapter;
 import silantyevmn.ru.materialdesign.model.photo.Photo;
 import silantyevmn.ru.materialdesign.model.theme.Theme;
 import silantyevmn.ru.materialdesign.presenter.GaleryPresenter;
-import silantyevmn.ru.materialdesign.view.fragment.PhotoFragment;
-import silantyevmn.ru.materialdesign.view.fragment.PhotoFragmentFavorite;
+import silantyevmn.ru.materialdesign.view.fragment.CustomFragmentPagerAdapter;
+import silantyevmn.ru.materialdesign.view.fragment.TabFragmentFactory;
 
 public class GaleryActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, IGaleryView {
@@ -35,9 +35,8 @@ public class GaleryActivity extends AppCompatActivity
     private final int IMPORT_GALERY_REQUEST = 2;
     private final int SETTING_REQUEST = 3;
     private DrawerLayout drawer;
-    private static final String PHOTO_FRAGMENT_FAVORITE_TAG = "photoFragmentFavorite";
-    private static final String PHOTO_FRAGMENT_HOME_TAG = "photoFragmentHome";
-    private IPhotoAdapter adapter;
+    private ViewPager mViewPager;
+    private CustomFragmentPagerAdapter customFragmentPagerAdapter;
 
     public GaleryActivity() {
         presenter = new GaleryPresenter(this);
@@ -99,7 +98,7 @@ public class GaleryActivity extends AppCompatActivity
     @Override
     public void showSetting(List<Theme> themes) {
         Intent intent = new Intent(GaleryActivity.this, SettingsActivity.class);
-        intent.putExtra(SettingsActivity.LIST_THEMES, (Serializable) themes);
+        //intent.putExtra(SettingsActivity.LIST_THEMES, (Serializable) themes);
         startActivityForResult(intent, SETTING_REQUEST);
     }
 
@@ -115,77 +114,39 @@ public class GaleryActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //создаем фабрику фрагментов
+        TabFragmentFactory tabFragmentFactory = new TabFragmentFactory();
+        customFragmentPagerAdapter = new CustomFragmentPagerAdapter(getSupportFragmentManager(), tabFragmentFactory);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(customFragmentPagerAdapter);
+
         //добавляем TabLayout
         TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        //вешаем принудительное обновление фрагментов по вкладкам
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                switch (position) {
-                    case 0: {
-                        presenter.selectFragmentHome();
-                        break;
-                    }
-                    case 1: {
-                        presenter.selectFragmentFavorite();
-                        break;
-                    }
-                    default:
-                        break;
-                }
+                presenter.onTabSelected(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                //
+
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                switch (position) {
-                    case 0: {
-                        presenter.selectFragmentHome();
-                        break;
-                    }
-                    case 1: {
-                        presenter.selectFragmentFavorite();
-                        break;
-                    }
-                    default:
-                        break;
-                }
+
             }
         });
-        //открываем сохраненный tab
+       /* //открываем сохраненный tab
         TabLayout.Tab tab = tabLayout.getTabAt(presenter.getIdFragment());
-        tab.select();
-    }
-
-    @Override
-    public void showFragmentFavorite(List<Photo> photos) {
-        PhotoFragmentFavorite photoFragment = (PhotoFragmentFavorite) getSupportFragmentManager().findFragmentByTag(PHOTO_FRAGMENT_FAVORITE_TAG);
-        if (photoFragment == null) {
-            photoFragment = new PhotoFragmentFavorite();
-            adapter = null;
-            //запускаем транзакцию и добавляем фрагмент
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_photo, photoFragment, PHOTO_FRAGMENT_FAVORITE_TAG)
-                    .commit();
-        }
-    }
-
-    @Override
-    public void showFragmentHome(List<Photo> photos) {
-        PhotoFragment photoFragment = (PhotoFragment) getSupportFragmentManager().findFragmentByTag(PHOTO_FRAGMENT_HOME_TAG);
-        if (photoFragment == null) {
-            photoFragment = new PhotoFragment();
-            adapter = (IPhotoAdapter) photoFragment;
-            //запускаем транзакцию и добавляем фрагмент
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_photo, photoFragment, PHOTO_FRAGMENT_HOME_TAG)
-                    .commit();
-        }
+        tab.select();*/
     }
 
     @Override
@@ -216,10 +177,8 @@ public class GaleryActivity extends AppCompatActivity
     }
 
     @Override
-    public void setAdapter(List<Photo> photos) {
-        if (adapter != null) {
-            adapter.setAdapter(photos);
-        }
+    public void updateAdapter() {
+        customFragmentPagerAdapter.updateAllFragmentsToAdapter();
     }
 
     @Override
@@ -250,4 +209,10 @@ public class GaleryActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public void showFullPhoto(Photo photo) {
+        Intent intent = new Intent(this, PhotoFullActivity.class);
+        intent.putExtra(PhotoFullActivity.PHOTO_FULL_ACTIVITY_KEY, photo.getUri());
+        startActivity(intent);
+    }
 }
