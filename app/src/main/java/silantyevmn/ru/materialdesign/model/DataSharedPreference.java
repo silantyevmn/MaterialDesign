cf8e6370504b737e708bf3fe39fb631370c7761d
silantyevmn <5992033@mail.ru>
2018-09-11 14:21:22 +0300

    add save setting_theme_position,setting_span_position,current_theme,current_span

--- app/src/main/java/silantyevmn/ru/materialdesign/model/DataSharedPreference.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/DataSharedPreference.java
@@ -11,8 +11,14 @@ import silantyevmn.ru.materialdesign.R;
  */
 
 public class DataSharedPreference {
-    private final String FRAGMENT_ID_KEY="fragment_id";
-    private final int FRAGMENT_ID_DEFAULT=0;
+    private final String SETTING_THEME_POSITION_KEY = "setting_theme_position_key";
+    private final int SETTING_THEME_POSITION_DEFAULT = 0;
+    private final String SETTING_SPAN_POSITION_KEY = "setting_span_position_key";
+    private final int SETTING_SPAN_POSITION_DEFAULT = 0;
+    private final String SPAN_CURRENT_KEY = "span_current_key";
+    private final int SPAN_CURRENT_DEFAULT = 2;
+    private final String FRAGMENT_ID_KEY = "fragment_id";
+    private final int FRAGMENT_ID_DEFAULT = 0;
     private final String SHARED_PREFERENCES_NAME = "setting";
     private final String THEME_ID_KEY = "theme_id_key";
     private final String CAMERA_URI_KEY = "photo_camera_key";
@@ -80,6 +86,36 @@ public class DataSharedPreference {
     }
 
     public int getIdFragment() {
-        return sharedSettings.getInt(FRAGMENT_ID_KEY,FRAGMENT_ID_DEFAULT);
+        return sharedSettings.getInt(FRAGMENT_ID_KEY, FRAGMENT_ID_DEFAULT);
+    }
+
+    public void setCurrentSpan(int span) {
+        sharedSettings.edit()
+                .putInt(SPAN_CURRENT_KEY, span)
+                .apply();
+    }
+
+    public int getCurrentSpan() {
+        return sharedSettings.getInt(SPAN_CURRENT_KEY, SPAN_CURRENT_DEFAULT);
+    }
+
+    public void setSettingSpanPosition(int spanPosition) {
+        sharedSettings.edit()
+                .putInt(SETTING_SPAN_POSITION_KEY, spanPosition)
+                .apply();
+    }
+
+    public int getSettingSpanPosition() {
+        return sharedSettings.getInt(SETTING_SPAN_POSITION_KEY, SETTING_SPAN_POSITION_DEFAULT);
+    }
+
+    public void setSettingCurrentThemePosition(int position) {
+        sharedSettings.edit()
+                .putInt(SETTING_THEME_POSITION_KEY, position)
+                .apply();
+    }
+
+    public int getSettingCurrentThemePosition() {
+        return sharedSettings.getInt(SETTING_THEME_POSITION_KEY, SETTING_THEME_POSITION_DEFAULT);
     }
 }
