0303d6f48a97ad7fc5886aa436d5e868efbfe5c2
silantyevmn <5992033@mail.ru>
2018-09-11 14:26:23 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/IGaleryView.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/IGaleryView.java
@@ -12,15 +12,13 @@ import silantyevmn.ru.materialdesign.model.theme.Theme;
 public interface IGaleryView {
     void showSetting(List<Theme> themes);
 
-    void showFragmentFavorite(List<Photo> photos);
-
-    void showFragmentHome(List<Photo> photos);
-
     void showImportCamera();
-
-    void setAdapter(List<Photo> list);
 
     void showImportGalery();
 
     void showLog(String title, String value);
+
+    void updateAdapter();
+
+    void showFullPhoto(Photo photo);
 }
