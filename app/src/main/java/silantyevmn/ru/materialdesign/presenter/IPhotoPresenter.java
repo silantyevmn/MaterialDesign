55be0a088d7d31f61b6002637b7beac5f71e24b6
silantyevmn <5992033@mail.ru>
2018-09-11 14:19:24 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/materialdesign/presenter/IPhotoPresenter.java
+++
@@ -1,8 +0,0 @@
-package silantyevmn.ru.materialdesign.presenter;
-
-/**
- * Created by silan on 30.08.2018.
- */
-
-public interface IPhotoPresenter {
-}
--- app/src/main/java/silantyevmn/ru/materialdesign/view/fragment/IPhotoFragmentUpdateAdapter.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/fragment/IPhotoFragmentUpdateAdapter.java
@@ -7,5 +7,5 @@ import java.util.List;
  */
 
 public interface IPhotoAdapter {
-    void setAdapter(List<Photo> photos);
+    void updateAdapter();
 }
