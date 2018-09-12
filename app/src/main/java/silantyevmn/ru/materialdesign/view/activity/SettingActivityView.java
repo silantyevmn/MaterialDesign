b507e74418e2e68ccf2eeef8bdcba18610a05460
silantyevmn <5992033@mail.ru>
2018-09-12 10:09:33 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/SettingActivityView.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/SettingActivityView.java
@@ -8,7 +8,8 @@ import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
  * Created by silan on 11.09.2018.
  */
 @StateStrategyType(AddToEndSingleStrategy.class)
-public interface SettingActivityView extends MvpView{
+public interface SettingActivityView extends MvpView {
     void init(int currentThemePosition, int currentSpanPosition);
+
     void finish();
 }
