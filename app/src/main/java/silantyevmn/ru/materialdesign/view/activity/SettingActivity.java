30175120622060e84cdcca51c82e784505f38d81
silantyevmn <5992033@mail.ru>
2018-09-12 10:08:43 +0300

    extend MvpAppCompatActivity

--- app/src/main/java/silantyevmn/ru/materialdesign/view/activity/SettingActivity.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/SettingActivity.java
@@ -1,10 +1,13 @@
 package silantyevmn.ru.materialdesign.view.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.arellomobile.mvp.presenter.ProvidePresenter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,7 +16,7 @@ import silantyevmn.ru.materialdesign.R;
 import silantyevmn.ru.materialdesign.model.DataSharedPreference;
 import silantyevmn.ru.materialdesign.presenter.SettingPresenter;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends MvpAppCompatActivity {
     @BindView(R.id.button_setting_action)
     Button button;
     @BindView(R.id.spinner_item_theme)
@@ -21,7 +24,15 @@ public class SettingsActivity extends AppCompatActivity {
     @BindView(R.id.spinner_item_span)
     Spinner spinnerSpan;
 
-    private SettingPresenter presenter;
+    @InjectPresenter
+    SettingPresenter presenter;
+
+    @ProvidePresenter
+    public SettingPresenter provideSettingPresenter() {
+        presenter = new SettingPresenter(this);
+        //TO SOMETHING WITH PRESENTER
+        return presenter;
+    }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -35,7 +46,7 @@ public class SettingsActivity extends AppCompatActivity {
         //добавляем в АппБар кнопку назад
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        presenter = new SettingPresenter(this);
+        presenter = provideSettingPresenter();
     }
 
     @OnClick(R.id.button_setting_action)
---
+++ app/src/main/java/silantyevmn/ru/materialdesign/view/activity/SettingActivityView.java
@@ -0,0 +1,14 @@
+package silantyevmn.ru.materialdesign.view.activity;
+
+import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+
+/**
+ * Created by silan on 11.09.2018.
+ */
+@StateStrategyType(AddToEndSingleStrategy.class)
+public interface SettingActivityView extends MvpView{
+    void init(int currentThemePosition, int currentSpanPosition);
+    void finish();
+}
