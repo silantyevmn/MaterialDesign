b90fb80f14a41d3dd977e9569d89ba062171533b
silantyevmn <5992033@mail.ru>
2018-10-26 18:45:45 +0300

    refactoring code rxJava2

--- app/src/main/java/silantyevmn/ru/materialdesign/presenter/PhotoPresenterFavorite.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/presenter/PhotoPresenterFavorite.java
@@ -9,6 +9,9 @@ import com.arellomobile.mvp.MvpPresenter;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
 import silantyevmn.ru.materialdesign.R;
 import silantyevmn.ru.materialdesign.model.DataSharedPreference;
 import silantyevmn.ru.materialdesign.model.photo.IPhotoModel;
@@ -17,7 +20,6 @@ import silantyevmn.ru.materialdesign.model.photo.PhotoModel;
 import silantyevmn.ru.materialdesign.view.DialogView;
 import silantyevmn.ru.materialdesign.view.activity.GaleryActivity;
 import silantyevmn.ru.materialdesign.view.activity.IGaleryView;
-import silantyevmn.ru.materialdesign.view.fragment.IPhotoFragment;
 import silantyevmn.ru.materialdesign.view.fragment.IPhotoFragmentFavorite;
 import silantyevmn.ru.materialdesign.view.fragment.PhotoFragmentFavorite;
 
@@ -26,23 +28,36 @@ import silantyevmn.ru.materialdesign.view.fragment.PhotoFragmentFavorite;
 public class PhotoPresenterFavorite extends MvpPresenter<IPhotoFragmentFavorite> {
     private final IPhotoModel model;
     private final IGaleryView mainActivity;
+    private Scheduler mainSheduler;
+    private List<Photo> photos;
 
-    public PhotoPresenterFavorite(PhotoFragmentFavorite photoFragment) {
+    public PhotoPresenterFavorite(PhotoFragmentFavorite photoFragment, Scheduler mainScheduler) {
         this.mainActivity = (GaleryActivity) photoFragment.getActivity();
         model = PhotoModel.getInstance();
+        this.mainSheduler = mainScheduler;
     }
 
     //создание View
     public void init(Context context) {
-        getViewState().init(getPhotos(), model.getGridLayoutManagerSpan(context.getResources().getConfiguration().orientation));
+        getPhotos().observeOn(mainSheduler)
+                .subscribe(list -> {
+                    photos = list;
+                    getViewState().init(list, model.getGridLayoutManagerSpan(context.getResources().getConfiguration().orientation));
+                });
+
     }
 
-    private List<Photo> getPhotos() {
-        return model.getListFavorite();
+    private Observable<List<Photo>> getPhotos() {
+        return model.getListFavorite().subscribeOn(Schedulers.io());
     }
 
     public void updateAdapter() {
-        getViewState().setAdapter(getPhotos());
+        getPhotos().observeOn(mainSheduler)
+                .subscribe(list -> {
+                    photos = list;
+                    getViewState().setAdapter(list);
+                });
+
     }
 
     public void delete(int position, Activity activity) {
@@ -53,21 +68,29 @@ public class PhotoPresenterFavorite extends MvpPresenter<IPhotoFragmentFavorite>
             return;
         }
         new DialogView(activity, activity.getString(R.string.dialog_title_delete), () -> {
-            model.delete(getPhotos().get(position));
-            updateAdapter();
-            getViewState().showLog("delete", String.valueOf(position));
+            model.delete(photos.get(position))
+                    .observeOn(mainSheduler)
+                    .subscribe(p -> {
+                        updateAdapter();
+                        getViewState().showLog("delete", String.valueOf(position));
+                    });
         });
 
     }
 
     public void favorite(int position) {
-        model.update(getPhotos().get(position));
-        updateAdapter();
-        getViewState().showLog("favourites", String.valueOf(position));
+        Photo photo = photos.get(position);
+        photo.setFavorite(!photo.isFavorite());
+        model.update(photo)
+                .observeOn(mainSheduler)
+                .subscribe(p -> {
+                    updateAdapter();
+                    getViewState().showLog("favourites", String.valueOf(position));
+                });
     }
 
     public void onClickPhoto(int adapterPosition) {
         //запускаем в главной активити
-        mainActivity.showFullPhoto(getPhotos().get(adapterPosition));
+        mainActivity.showFullPhoto(photos.get(adapterPosition));
     }
 }
