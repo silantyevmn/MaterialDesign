47542ca2e08d3a897b473d1d5ef46b2f99370fc5
silantyevmn <5992033@mail.ru>
2018-10-26 18:41:15 +0300

    interface ICache

--- app/src/main/java/silantyevmn/ru/materialdesign/model/cache/ICache.java
+++ app/src/main/java/silantyevmn/ru/materialdesign/model/cache/ICache.java
@@ -1,8 +1,19 @@
 package silantyevmn.ru.materialdesign.model.cache;
 
-/**
- * Created by silan on 21.10.2018.
- */
+import java.util.List;
+import io.reactivex.Observable;
+
+import silantyevmn.ru.materialdesign.model.photo.Photo;
+
 
 public interface ICache {
+    Observable<List<Photo>> getList();
+
+    Observable<List<Photo>> getListFavorite();
+
+    Observable insert(Photo photo);
+
+    Observable delete(Photo photo);
+
+    Observable update(Photo photo);
 }
